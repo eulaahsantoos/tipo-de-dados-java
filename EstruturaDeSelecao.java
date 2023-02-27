a47c74f34a68c48965e1edde0dcaa0af80de1be5
eulaahsantoos <laizasantos616@gmail.com>
2023-02-27 15:15:16 +0000

    salve.aula2

---
+++ EstruturaDeSelecao.java
@@ -0,0 +1,20 @@
+public class EstruturaDeSelecao{
+
+    public static void main(String[] args){ 
+
+        int idade;
+
+        idade=19;
+
+        if (idade >= 18)
+        {
+            System.out.print("apto ao serviço militar obrigatorio");
+} else {
+     System.out.print("Não esta opto ao serviço militar obrigatorio!");
+
+}
+
+}
+
+
+}
